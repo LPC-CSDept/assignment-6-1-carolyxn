c3edbd4e7580ee71a1fd63f6569b6766edc605b5
Carolyn <carol.nguyen2007@gmail.com>
2022-10-04 21:39:13 -0700

    yay

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std; 
+
+int inputValid(int, int); // call functions
+void prime(int, int);
+
+int main ()
+{
+    int begin, end, y; 
+
+    cout << "Enter in your range from [from, to]: " << endl;
+    cin >> begin >> end;
+    
+    y = inputValid(begin, end);// validation
+    if (!y)
+        exit(0);
+    prime(begin, end); 
+    
+}
+
+int inputValid(int a, int b) // input validation function
+{
+    if (a > b || a < 2 || b < 0)
+        return 0;
+    else 
+        return 1;
+}
+
+void prime(int begin, int end) // calculation function, returns 0 or 1
+{
+    int x, y;
+    for (x = begin; x <= end; x++)
+    {
+        for (y = 2; y <= (static_cast<float>(x) / 2); y++){
+            if (x % y == 0)
+                break;
+            }
+        if (y > (static_cast<float>(x)/2))
+            cout << "Prime numbers: " << x << endl;
+            
+    }
+}
