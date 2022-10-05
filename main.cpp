0536b3393a89b30bd3f5a26de29299414c0fb2db
Carolyn <carol.nguyen2007@gmail.com>
2022-10-04 21:46:54 -0700

    yay

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int main ()
 
 int inputValid(int a, int b) // input validation function
 {
-    if (a > b || a < 2 || b < 0)
+    if (a >= b || a < 2 || b < 0)
         return 0;
     else 
         return 1;
@@ -31,11 +31,14 @@ void prime(int begin, int end) // calculation function, returns 0 or 1
     int x, y;
     for (x = begin; x <= end; x++)
     {
-        for (y = 2; y <= (static_cast<float>(x) / 2); y++){
+        if (x == 0 || x == 1){
+            continue;
+        }
+        for (y = 2; y <= (x / 2); y++){
             if (x % y == 0)
                 break;
             }
-        if (y > (static_cast<float>(x)/2))
+        if (y > (x/2))
             cout << "Prime numbers: " << x << endl;
             
     }
