c8da70bb9d930a56c19864d90ef1448461d0f18d
Carolyn <carol.nguyen2007@gmail.com>
2022-10-05 21:10:38 -0700

    huh

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std; 
 
 int inputValid(int, int); // call functions
-void prime(int, int);
+void getListprime(int, int);
 
 int main ()
 {
@@ -12,34 +12,43 @@ int main ()
     cin >> begin >> end;
     
     y = inputValid(begin, end);// validation
-    if (!y)
+    if (y == 0)
         exit(0);
-    prime(begin, end); 
+    getListprime(begin, end); 
     
 }
 
-int inputValid(int a, int b) // input validation function
+int inputValid(int begin, int end) // input validation function
 {
-    if (a >= b || a < 2 || b < 0)
+    if (begin > end) || (begin < 2) || (end < 0)
         return 0;
     else 
         return 1;
 }
 
-void prime(int begin, int end) // calculation function, returns 0 or 1
+void getListprime(int begin, int end) // calculation function, returns 0 or 1
 {
-    int x, y;
-    for (x = begin; x <= end; x++)
+    for (j=2; j<=(i/2); j++)
     {
-        if (x == 0 || x == 1){
-            continue;
-        }
-        for (y = 2; y <= (x / 2); y++){
-            if (x % y == 0)
-                break;
-            }
-        if (y > (x/2))
-            cout << "Prime numbers: " << x << endl;
-            
+        if i % j == 0
+            break;
+        if j > (i/2)
+            cout << i << "Prime number\n";
+        return(0)
     }
 }
+
+// int x, y;
+//     for (x = begin; x <= end; x++)
+//     {
+//         if (x == 0 || x == 1){
+//             continue;
+//         }
+//         for (y = 2; y <= (x / 2); y++){
+//             if (x % y == 0)
+//                 break;
+//             }
+//         if (y > (x/2))
+//             cout << "Prime numbers: " << x << endl;
+            
+//     }
