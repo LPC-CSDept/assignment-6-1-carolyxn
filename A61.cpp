214e7b8c557140bdd77379fac1b75739990e7cee
carolyxn <112140403+carolyxn@users.noreply.github.com>
2022-10-13 23:02:34 -0700

    oops

--- A61.cpp
+++ A61.cpp
@@ -1,91 +1,42 @@
 #include <iostream>
-using namespace std;
-
-
-int main()
+using namespace std; 
+int inputValid(int, int); // call functions
+void prime(int, int);
+int main ()
 {
-	int pnum1, pnum2, pnum3;
-    int pnum1a, pnum2a, pnum3a;
-
-	/*********************************
-	Make Your Code
-	*********************************/
-	
-	cout << "Enter in a prime number: " << endl;
-	cin >> pnum1;
+    int begin, end, y; 
+    cout << "Enter in your range from [from, to]: " << endl;
+    cin >> begin >> end;
     
-    while (pnum1 < 0)
-		{
-			cout << "Invalid: Enter in a prime number: " << endl;
-			cin >> pnum1;
-			pnum1a *= -1;
-            for(int i = 2; i <= (pnum1a/2); i++)
-			{
-				if (pnum1a % i == 0 || pnum1a != 1)
-					break;
-			}
-		}
-	for(int i = 2; i<=(pnum1/2); i++)
-		{
-			while ( pnum1 % i != 0 || pnum1 == 1)
-			{
-				cout << "Invalid: Enter in a prime number: " << endl;
-				cin >> pnum1;
-			}
-			if (pnum1 % i == 0 || pnum1 != 1)
-				break;
-		}
+    y = inputValid(begin, end);// validation
+    while (!y)
+    {
+        cout << "invalid input." << endl;
+        cout << "Enter in your range from [from, to]: " << endl;
+        cin >> begin >> end;
+        y = inputValid(begin, end); // validation
+    }
+    prime(begin, end); 
     
-    
-	cout << "Enter in a prime number: " << endl;
-	cin >> pnum2;
-
-    while (pnum2 < 0)
-		{
-			cout << "Invalid: Enter in a prime number: " << endl;
-			cin >> pnum2;
-			pnum2a *= -1;
-            for(int i =2; i<= (pnum2a/2); i++)
-			{
-				if (pnum2a % i != 0 || pnum2a != 1)
-				break;
-			}
-		}
-	for(int i =2; i<pnum2; i++)
-		{
-			while ( pnum2 % i != 0 || pnum2 == 1)
-			{
-				cout << "Invalid: Enter in a prime number: " << endl;
-				cin >> pnum2;
-			}
-			if (pnum2 % i == 0 || pnum2 != 1)
-				break;
-		}
-    
-	cout << "Enter in a prime number: " << endl;
-	cin >> pnum3;
-    while (pnum3 < 0)
-		{
-			cout << "Invalid: Enter in a prime number: " << endl;
-			cin >> pnum3;
-			pnum3a *= -1;
-            for(int i =2; i<=(pnum3a/2); i++)
-        	{
-				if (pnum3a % i != 0 || pnum3a != 1)
-				break;
-			}
-		}
-	for(int i =2; i<= (pnum3/2); i++)
-        {
-			while ( pnum3 % i != 0 || pnum3 == 1)
-			{
-				cout << "Invalid: Enter in a prime number: " << endl;
-				cin >> pnum3;
-			}
-			if (pnum3 % i == 0 || pnum3 != 1)
-				break;
-		}
-    
-    
-	cout << "Prime numbers: " << pnum1 << ", " << pnum2 << ", " << pnum3;
+}
+int inputValid(int a, int b) // input validation function
+{
+    if (a >= b || a < 0 || b < 0)
+        return 0;
+    else 
+        return 1;
+}
+void prime(int begin, int end) // calculation function, returns 0 or 1
+{
+    int x, y;
+    for (x = begin; x <= end; x++)
+    {
+        for (y = 2; y <= (x / 2); y++){
+            if (x % y == 0)
+                break;
+            }
+        if (y > (static_cast<double>(x)/2))
+            cout << "Prime numbers: " << x << endl;
+            
+    }
 }
